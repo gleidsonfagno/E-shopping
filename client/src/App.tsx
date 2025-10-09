96334efa1e536429003025a3524f3e41c2493989
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-10-09 15:51:20 -0300

    projeto iniciado

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <>
+    <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ullam doloremque libero, esse iure odio amet temporibus veniam, labore, unde exercitationem? Obcaecati quasi et dignissimos hic eligendi tenetur ipsam soluta!</h1>
+    </>
+  )
+}
+
+export default App
